b71de3f3f07f1c381f1d0e005936a06bb6459767
Sachin Yadav <sachin@camcom.ai>
2023-02-27 18:17:06 +0000

    modifie done

--- dbr_file.py
+++ dbr_file.py
@@ -5,14 +5,4 @@ good morning
 <<<<<<< HEAD
 Welcome to github
 On branch main
-Your branch is up to date with 'origin/main'.
-
-Changes not staged for commit:
-  (use "git add <file>..." to update what will be committed)
-  (use "git restore <file>..." to discard changes in working directory)
-	modified:   dbr_file.py
-
-no changes added to commit (use "git add" and/or "git commit -a")
-=======
 Ba khuda jee na skumga ek lamha arama ata hai didar se
->>>>>>> 8c762a75b730172c805206328273f4e15eb0b7dd
